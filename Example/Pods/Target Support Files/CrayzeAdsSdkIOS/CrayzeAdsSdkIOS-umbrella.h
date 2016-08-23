5feab9c88b6b3fc8a1e520a46ad617ef97477e94
Amitay Molko <amitaymolko@gmail.com>
2016-08-23 10:30:53 +0300

    Initial Commit

---
+++ Example/Pods/Target Support Files/CrayzeAdsSdkIOS/CrayzeAdsSdkIOS-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double CrayzeAdsSdkIOSVersionNumber;
+FOUNDATION_EXPORT const unsigned char CrayzeAdsSdkIOSVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-CrayzeAdsSdkIOS_Tests/Pods-CrayzeAdsSdkIOS_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_CrayzeAdsSdkIOS_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_CrayzeAdsSdkIOS_TestsVersionString[];
+
